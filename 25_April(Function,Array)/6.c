849802fbe3c4ea2b3d9390eb3808430cb4407d29
Subham Moharana <121431904+SubhammMoharana@users.noreply.github.com>
2023-04-30 10:08:18 +0530

    6.c Modified

--- 25_April(Function,Array)/6.c
+++ 25_April(Function,Array)/6.c
@@ -1,30 +1,28 @@
-//write a program input element to an array and display the second biggest element#include <stdio.h>
+// write a program input element to an array and display the second biggest element
 #include <stdio.h>
-int main() {
-  int size, largest, second_largest;
-  
-  // input the size of the array
-  printf("Enter the size of the array: ");
-  scanf("%d", &size);
-  
-  // input elements to the array and find the largest and second largest elements
-  int arr[size];
-  printf("Enter the elements of the array:\n");
-  for (int i = 0; i < size; i++) {
+int main()
+{
+  int max1, max2, arr[5];
+  // input
+  printf("Enter the array of 5 elemnts");
+  for (int i = 0; i < 5; i++)
+  {
     scanf("%d", &arr[i]);
-    if (i == 0) {
-      largest = arr[i];
-      second_largest = arr[i];
-    } else if (arr[i] > largest) {
-      second_largest = largest;
-      largest = arr[i];
-    } else if (arr[i] > second_largest) {
-      second_largest = arr[i];
+  }
+  // logic for second highest number
+  max1 = max2 = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    if (arr[i] > max1)
+    {
+      max2 = max1;
+      max1 = arr[i];
+    }
+    else if (arr[i] > max2 && arr[i] < max1)
+    {
+      max2 = arr[i];
     }
   }
-  
-  // print out the second largest element
-  printf("The second largest element is: %d", second_largest);
-  
+  printf("The second largest element is %d",max2);
   return 0;
 }
